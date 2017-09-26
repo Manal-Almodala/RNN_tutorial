b4b3e5a0d9bc9ef176e906b6f12f095b59102be9
aerin <ohmyheroine@hotmail.com>
2017-09-25 23:24:46 -0700

    run_from_saved.py file is added

--- run_from_saved.py
+++ run_from_saved.py
@@ -1,3 +1,5 @@
+from utils import load_model_parameters_theano, save_model_parameters_theano
+from rnn_theano import * 
 import csv
 import itertools
 import operator
@@ -7,9 +9,7 @@ import sys
 from datetime import datetime
 from utils import *
 import matplotlib.pyplot as plt
-import tensorflow as tf 
-
-nltk.download("book")
+ 
 
 vocabulary_size = 8000
 unknown_token = "UNKNOWN_TOKEN"
@@ -61,44 +61,42 @@ print "x:\n%s\n%s" % (" ".join([index_to_word[x] for x in x_example]), x_example
 print "\ny:\n%s\n%s" % (" ".join([index_to_word[x] for x in y_example]), y_example)
 
 
-hidden_dim = 100
-vocab_size = 8000
+vocabulary_size=8000
+
+
+model = RNNTheano(vocabulary_size, hidden_dim=80)
+# losses = train_with_sgd(model, X_train, y_train, nepoch=50)
+# save_model_parameters_theano('./data/trained-model-theano.npz', model)
+
+load_model_parameters_theano('./data/rnn-theano-80-8000-2017-09-25-10-41-00.npz', model)
+#load_model_parameters_theano('./data/trained-model-theano.npz', model)
+
+def generate_sentence(model):
+    # We start the sentence with the start token
+    new_sentence = [word_to_index[sentence_start_token]]
+    # Repeat until we get an end token
+    while not new_sentence[-1] == word_to_index[sentence_end_token]:
+        next_word_probs = model.forward_propagation(new_sentence)
+        sampled_word = word_to_index[unknown_token]
+        # We don't want to sample unknown words
+        while sampled_word == word_to_index[unknown_token]:
+            samples = np.random.multinomial(1, next_word_probs[-1])
+            sampled_word = np.argmax(samples)
+        new_sentence.append(sampled_word)
+    sentence_str = [index_to_word[x] for x in new_sentence[1:-1]]
+    return sentence_str
+
+num_sentences = 10
+senten_min_length = 7
+
+for i in range(num_sentences):
+    sent = []
+    # We want long sentences, not sentences with one or two words
+    while len(sent) < senten_min_length:
+        sent = generate_sentence(model)
+    print " ".join(sent)
 
 
 
 
-graph = tf.Graph()
-with graph.as_default():
-	#Parameters
-	U = tf.Variable(tf.truncated_normal([hidden_dim, vocab_size]), -0.1, 0.1)
-	W = tf.Variable(tf.truncated_normal([hidden_dim, hidden_dim]), -0.1, 0.1)
-	V = tf.Variable(tf.truncated_normal([vocab_size, hidden_dim]), -0.1, 0.1)
 
-	def forward_propagation(X):
-		# input  : list of index of words
-		# output : hidden state & output (probability of next words) 
-		T = len(X)
-		S = tf.zeros((T+1,hidden_dim))
-		O = tf.zeros((T,vocab_size))
-		
-		for t in xrange(T):
-			S[t] = tf.tanh( U[:,X[t]] + tf.matmul(W,S[t-1]) )
-			O[t] = tf.nn.softmax(tf.matmul(V,S))
-		return S, O
-
-	def predict(X):
-		# Returns maximum argument and output
-		s, o = forward_propagation(X)
-		return np.argmax(o, axis=1), o
-
-	def calculate_total_loss(X, y):
-		y_hat, o = predict(X)
-		for i in xrange(y):
-			loss += -np.log(o[i][y[i]]) 
-		return loss 
-
-it to 1000 examples to save time
-print "Expected Loss for random predictions: %f" % np.log(vocabulary_size)
-print "Actual loss: %f" % model.calculate_loss(X_train[:1000], y_train[:1000])
-	
-	
